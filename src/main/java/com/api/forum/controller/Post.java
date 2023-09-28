3983643a8e05d8b8b49da6af43a7abeed0b42bc2
Administrator <Administrator@MAY41.MAY29>
2023-09-28 21:19:19 +0700

    bug fix for controller

--- src/main/java/com/api/forum/controller/Post.java
+++ src/main/java/com/api/forum/controller/Post.java
@@ -4,18 +4,24 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.forum.constants.App;
 import com.api.forum.payload.CommentDTO;
 import com.api.forum.payload.PostDTO;
+import com.api.forum.payload.PostDTOPaged;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
@@ -28,13 +34,13 @@ public class Post {
             description = "Http Status 200 SUCCESS"
     )
     @GetMapping
-    public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    public PostDTOPaged getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = App.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = App.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = App.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = App.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ){
-        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+        return this.service.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     @ApiResponse(
@@ -57,8 +63,8 @@ public class Post {
     // create blog post rest api
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDto dto){
-        return new ResponseEntity<PostDTO>(postService.createPost(dto), HttpStatus.CREATED);
+    public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO dto){
+        return new ResponseEntity<PostDTO>(this.service.createPost(dto), HttpStatus.CREATED);
     }
 
 
