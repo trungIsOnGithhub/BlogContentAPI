4d5830469dd6e1c5e7c7a14061d1b2599a27ffc2
trung <trung@gmail.com>
2023-09-28 17:16:29 +0700

    add security for post

--- src/main/java/com/api/forum/controller/Post.java
+++ src/main/java/com/api/forum/controller/Post.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.forum.constants.App;
 import com.api.forum.payload.CommentDTO;
 import com.api.forum.payload.PostDTO;
 
@@ -21,11 +22,45 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/v1")
 public class Post {
 	private com.api.forum.service.Post service;
-	
+
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @GetMapping
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    @ApiResponse(
+        responseCode = "200",
+        description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/posts/{postId}")
     public ResponseEntity<PostDTO> getPostById(@PathVariable(value = "postId") Long postId){
         return new ResponseEntity<PostDTO>(this.service.getPostById(postId), HttpStatus.OK);
     }
+
+  
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    // create blog post rest api
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDto dto){
+        return new ResponseEntity<PostDTO>(postService.createPost(dto), HttpStatus.CREATED);
+    }
+
 
     @PutMapping("/posts/{postId}")
     public ResponseEntity<PostDTO> updatePost(@Valid @RequestBody PostDTO PostDTO, @PathVariable(name = "postId") long id){
@@ -35,6 +70,14 @@ public class Post {
        return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
+    @ApiResponse(
+        responseCode = "200",
+        description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/posts/{postId}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "postId") long id){
 
