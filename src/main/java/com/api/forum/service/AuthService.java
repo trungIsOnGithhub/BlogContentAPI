b77506b8a455e31aaa4167675f935b1389cda1bc
trungvuive <trung150502@gmail.com>
2023-09-30 22:15:40 +0700

    rename file for better control

--- src/main/java/com/api/forum/service/AuthService.java
+++ src/main/java/com/api/forum/service/AuthService.java
@@ -3,7 +3,7 @@ package com.api.forum.service;
 import com.api.forum.payload.LoginDTO;
 import com.api.forum.payload.RegisterDTO;
 
-public interface Auth {
+public interface AuthService {
     public String login(LoginDTO loginDTO);
     public String register(RegisterDTO registerDTO);
 }
