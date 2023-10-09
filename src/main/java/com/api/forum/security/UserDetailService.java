0135e5e7bdf82a90e338d8d1e6353eba33733fb7
Trung Nguyen <trung150502@gmail.com>
2023-10-09 04:16:27 +0000

    spring user detail service

---
+++ src/main/java/com/api/forum/security/UserDetailService.java
@@ -0,0 +1,32 @@
+package com.api.forum.security;
+
+import com.api.forum.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository repository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.repository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        com.api.forum.entity.User user = this.repository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                 .orElseThrow( () -> new UsernameNotFoundException("User not found with username or email: "+ usernameOrEmail) );
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                                                .map( (role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet() );
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
