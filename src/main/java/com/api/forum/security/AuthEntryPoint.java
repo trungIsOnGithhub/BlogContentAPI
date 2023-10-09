2424dc1aa6ee943c6d90994b5f295b4a7fa76ea1
Trung Nguyen <trung150502@gmail.com>
2023-10-09 04:15:12 +0000

    authentication entry point

---
+++ src/main/java/com/api/forum/security/AuthEntryPoint.java
@@ -0,0 +1,20 @@
+package com.api.forum.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AuthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws ServletException, IOException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
